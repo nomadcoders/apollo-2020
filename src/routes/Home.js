e69db9c217a24fa67cf6bef8bd4c5c3bc6e4f8b8
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-21 20:35:01 +0900

    #1.1 GET_MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,22 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map(m => <h1>{m.id}</h1>);
+  }
+};
