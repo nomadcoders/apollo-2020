7f76cd913858a46143b1d2d80c64a0cb1d7eb14a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-21 20:28:35 +0900

    #1.0 Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from "./apollo";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
