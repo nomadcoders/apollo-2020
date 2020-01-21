81a6596063d82391439fdf3e1e5795a177b673b1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-21 20:22:15 +0900

    #0.3 Router and Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/:id" component={Detail} />
+    </Router>
+  );
 }
 
 export default App;
